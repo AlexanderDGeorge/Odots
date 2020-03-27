7bf65119f3b35c67c6cd4c097331a3f66a9396f0
Alexander George <alexander97.george@gmail.com>
2020-03-26 17:19:56 -0700

    push to heroku

--- server/server.js
+++ server/server.js
@@ -15,11 +15,11 @@ if (!database) {
 }
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('frontend/build'));
+    app.use(express.static('client/build'));
     app.get('/', (req, res) => {
-      res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     })
-}
+  }
 
 mongoose
   .connect(database, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
